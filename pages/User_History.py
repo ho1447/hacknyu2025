166a2612c1b4451312ed5d414760c84983086a21
Iris H <irish@iriss-mbp.mynetworksettings.com>
2025-02-08 22:50:40 -0500

    Added sidebar info

--- pages/User_History.py
+++ pages/User_History.py
@@ -2,7 +2,6 @@ import streamlit as st
 from pymongo import MongoClient 
 import pandas as pd
 
-connection_string = ""
 client = MongoClient(connection_string)
 db = client["hacknyu2025"]
 collection = db["patient"]
@@ -13,11 +12,8 @@ def main():
     result = collection.find({"patient_id": 1},{ "_id": 0, "patient_id": 0, "user_id": 0, "created_at": 0 })
     df = pd.DataFrame(list(result))
     st.dataframe(df)
-    # if "userHistory" not in st.session_state:
-    #     st.session_state["userHistory"] = [
-    #     ]
-    # for res in result:
-    #     st.session_state.userHistory.append(list(result))
+    for res in result:
+        st.session_state.userHistory.append(list(result))
     
 
 if __name__ == "__main__":
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
+from pymongo import MongoClient 
+import pandas as pd
 
 # initialization
-GEMINI_API_KEY=""
 llm = ChatGoogleGenerativeAI(
     model="gemini-1.5-pro",
     temperature=0,
@@ -11,6 +12,21 @@ llm = ChatGoogleGenerativeAI(
     max_retries=2,
     api_key=GEMINI_API_KEY
 )
+connection_string = "mongodb+srv://phh242:hacknyu2025password@hacknyu2025.1lcf2.mongodb.net/?retryWrites=true&w=majority&appName=hacknyu2025"
+client = MongoClient(connection_string)
+db = client["hacknyu2025"]
+collection = db["patient"]
+illness = db["illness_history"]
+vaccination = db["vaccination_history"]
+
+if "userHistory" not in st.session_state:
+    result = collection.find({"patient_id": 1},{ "_id": 0, "patient_id": 0, "user_id": 0, "created_at": 0 })
+    r1 = illness.find({"patient_id": 1},{ "_id": 0, "patient_id": 0, "illness_id": 0, "created_at": 0 })
+    vacc = vaccination.find({"patient_id": 1},{ "_id": 0, "patient_id": 0, "vaccination_id": 0, "vaccine_batch": 0, "created_at": 0 })
+    st.session_state["userHistory"] = {}
+    for res in result:
+        st.session_state.userHistory.update(res)
+    print(st.session_state.userHistory)
 
 def generate_response(input_text):    
     # st.info(llm.invoke(input_text))
@@ -20,9 +36,13 @@ def generate_response(input_text):
 
 def main():
 
-    st.title("🦜🔗 Quickstart App")
+    st.title("SicklySage")
 
-    # with st.sidebar:
+    with st.sidebar:
+        st.metric("Emergency Contact", st.session_state.userHistory["emergency_contact"])
+        with st.expander("See Illnesses"):
+            st.write(["Amnesia", "Diabetes"])
+            # st.metric("Emergency Contact", st.session_state.userHistory["emergency_contact"])
         # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
         # "[![Open GitHub Repository](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
@@ -38,16 +58,14 @@ def main():
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = [
-            {"role": "assistant", "content": "Hi, I'm a chatbot who can search the web. How can I help you?"}
+            {"role": "assistant", "content": "Hi, SicklySage is here to answer your questions about any symptoms you have! How can I help you?"}
         ]
 
     for msg in st.session_state.messages:
         st.chat_message(msg["role"]).write(msg["content"])
 
     if prompt := st.chat_input(placeholder="Ask a question"):
-        for history in st.session_state.userHistory:
-            for hist in history:
-                prompt = prompt + " " + hist
+        prompt = prompt + " " + st.session_state.userHistory["emergency_contact"]
         print(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.chat_message("user").write(prompt)
